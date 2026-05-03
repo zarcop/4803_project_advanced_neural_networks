001cf09f54c2a796bc9a0cff1809f6f118e3558d
Elias Zarco <eliaszarco@gmail.com>
2026-05-03 17:10:33 -0400

    initial alexnet structure

--- model/alexnet.py
+++ model/alexnet.py
@@ -1,4 +1,4 @@
-from torch import nn, relu
+from torch import nn
 
 
 class AlexNet4803(nn.Module):
@@ -7,11 +7,19 @@ class AlexNet4803(nn.Module):
     def init(self, in_channels = 32, k_classes = 10):
         super().__init__()
         self.convolutional_block = nn.Sequential(
-            nn.Conv2d(in_channels, 96, kernel_size=5, padding = 2, stride = 1),
+            nn.Conv2d(in_channels, 96, kernel_size=5, padding = 2, stride = 4),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size =  3 , stride = 2, padding = 1),
             nn.LocalResponseNorm(size = 5),
             nn.Conv2d(),
+            nn.ReLU(),
+            nn.Conv2d(),
+            nn.ReLU(),
+            nn.Conv2d(),
+            nn.ReLU(),
+        )
 
+        self.fully_connected_layer = nn.Sequential(
+            
         )
 
