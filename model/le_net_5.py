485b5becff8c27c52ce7637f19ba335fef793192
Elias Zarco <eliaszarco@gmail.com>
2026-05-05 23:08:21 -0400

    le net modifications to include dropout rate

--- model/le_net_5.py
+++ model/le_net_5.py
@@ -7,28 +7,29 @@ from torch import nn
 # this works both with CIFAR-10 and MNIST datasets.
 
 class LeNet5(nn.Module):
-    def __init__(self, in_channels = 32, k_classes = 10): # probably going to be 10 with CIFAR-10
-        #convolutional block
-        super.__init__()
+    def __init__(self, in_channels=1, k_classes=10, dropout=0.0):
+        super().__init__()
+
         self.convolutional_block = nn.Sequential(
-            nn.Conv2d(in_channels, 6, kernel_size=5, stride = 1),
+            nn.Conv2d(in_channels, 6, kernel_size=5, stride=1),
             nn.Tanh(),
             nn.AvgPool2d(kernel_size=2, stride=2),
             nn.Conv2d(6, 16, kernel_size=5),
             nn.Tanh(),
-            nn.AvgPool2d(kernel_size=2, stride=1),
+            nn.AvgPool2d(kernel_size=2, stride=2),
             nn.AdaptiveAvgPool2d((5, 5)),
-            nn.Flatten()
+            nn.Flatten(),
         )
-        #fully connected blocks
-        self.fully_connected_block  =  nn.Sequential(
+        self.fully_connected_block = nn.Sequential(
             nn.Linear(16 * 5 * 5, 120),
             nn.Tanh(),
+            nn.Dropout(p=dropout),
             nn.Linear(120, 84),
             nn.Tanh(),
-            nn.Linear(84, k_classes)
+            nn.Dropout(p=dropout),
+            nn.Linear(84, k_classes),
         )
-    def forward(self,x):
+    def forward(self, x):
         x = self.convolutional_block(x)
         x = self.fully_connected_block(x)
         return x
